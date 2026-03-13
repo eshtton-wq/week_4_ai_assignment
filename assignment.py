443232a95e1de262fd4acd8371ec12ab60714768
eshtton-wq <eshtton@gmail.com>
2026-03-12 21:55:51 -0700

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,16 @@
+greet_students = "Hello AI Students"
+print(greet_students)
+
+name = "John"
+nge = 20
+city = "Nairobi"
+
+programing_languages = ["python", "JavaSript", "C#", "C++", "Java"]
+print(programing_languages[0])
+
+student = {
+  "Name" : "John Doe",
+  "Age" : 21,
+  "Course" : "AI"
+}
+print(student["Name"])
